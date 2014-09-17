db45525601e7b9c954379a4ea6e7d78b13f43f8d
James Vannordstrand <james.vannordstrand@banno.com>
2014-09-16 19:51:15 -0500

    adding sliding menu and few animations for nicer look

--- js/frontend.js
+++ js/frontend.js
@@ -12,10 +12,6 @@ $(function() {
  );
 });
 
-$(document).ready(function() {
-  setInterval ('cursorAnimation()', 600);
-});
-
 function cursorAnimation() {
   $('#cursor').animate({
     opacity: 0
@@ -29,29 +25,81 @@ var bio = ["I'm an undergraduate at the University of Northern Iowa. I love lear
 var homepage = true;
 
 function populateHome() {
-  document.getElementById("aSwitch").style.backgroundImage="url('images/contact.png')";
-  document.getElementById("aSwitch").title = "Contact Me";
-  document.getElementById("p1").innerHTML = bio[0];
+  $('#p1').fadeTo("medum", 0.01);
+  document.getElementById('p1').innerHTML = bio[0];
+  $('#p1').fadeTo("medum", 1.00);
+  $('#p2').fadeTo("medum", 0.01);
   document.getElementById("p2").innerHTML = bio[1];
+  $('#p2').fadeTo("medum", 1.00);
+  $('#p3').fadeTo("medum", 0.01);
   document.getElementById("p3").innerHTML = bio[2];
+  $('#p3').fadeTo("medum", 1.00);
 }
 
 function populateAbout() {
-  document.getElementById("aSwitch").style.backgroundImage = "url('images/home.png')";
-  document.getElementById("aSwitch").title = "Home";
+  $('#p1').fadeTo("medium", 0.01);
   document.getElementById("p1").innerHTML = "Phone Number: 319-415-2243";
+  $('#p1').fadeTo("medium", 1.00);
+  $('#p2').fadeTo("medium", 0.01);
   document.getElementById("p2").innerHTML = "Email: jamesvannordstrand@yahoo.com";
+  $('#p2').fadeTo("medium", 1.00);
+  $('#p3').fadeTo("medium", 0.01);
   document.getElementById("p3").innerHTML = "";
+  $('#p3').fadeTo("medium", 1.00);
 }
 
 function switchPage() {
   if (homepage){
     populateAbout();
     homepage = false;
+    eraseThenType('#switchText', 'Home');
   } else {
     populateHome();
     homepage = true;
+    eraseThenType('#switchText', 'Contact');
   }
 }
+
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+    body = document.body;
+
+showLeft.onclick = function() {
+  classie.toggle( this, 'active' );
+  classie.toggle( menuLeft, 'cbp-spmenu-open' );
+};
+
+$(document).ready(function() {
+  setInterval ('cursorAnimation()', 600);
+});
+
+// erasing stuff and typing stuff
+function eraseThenType(id, cap) {
+  captionEl     = $(id);
+  oldCaption    = captionEl.html();
+  newCaption    = cap;
+  captionLength = oldCaption.length;
+
+  erase()
+  setTimeout('type()', 800);
+}
+
+function erase() {
+  captionEl.html(oldCaption.substr(0, captionLength--));
+  if(captionLength >= 0) {
+    setTimeout('erase()', 50);
+  } else {
+    captionLength = 0;
+  }    
+}
+ 
+function type() {
+  captionEl.html(newCaption.substr(0, captionLength++));
+  if(captionLength < newCaption.length+1) {
+    setTimeout('type()', 50);
+  } else {
+    captionLength = 0;
+  }
+}
+///////////////////////////
 
 window.onload = populateHome;
